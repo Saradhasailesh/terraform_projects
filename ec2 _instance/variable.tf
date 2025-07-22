d646f5bf373973404908f538570a8992decc8cae
Saradha Boothalingham <saradha.b@ebmpapst.local>
2025-07-22 16:51:34 +0530

    Respect updated .gitignore

---
+++ ec2 _instance/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
